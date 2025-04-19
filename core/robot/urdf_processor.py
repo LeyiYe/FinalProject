d6463b3ced58e57b6c57adb5f076ef4a6cf7967a
LeyiYe <leyiasbarxas@gmail.com>
2025-04-19 18:38:13 +0100

    .

--- core/robot/urdf_processor.py
+++ core/robot/urdf_processor.py
@@ -5,6 +5,7 @@ from transforms3d.euler import euler2mat
 class URDFProcessor:
     def __init__(self, urdf_file):
         self.tree = ET.parse(urdf_file)
+        self.root = self.tree.getroot()
         self.links = self._parse_links()
         self.joints = self._parse_joints()
         
@@ -14,7 +15,6 @@ class URDFProcessor:
             link_name = link.get('name')
             collisions = []
             
-            # Process all collision geometries
             for collision in link.findall('collision'):
                 geo = collision.find('geometry')
                 origin = collision.find('origin')
@@ -28,16 +28,14 @@ class URDFProcessor:
                     rot = euler2mat(*[float(x) for x in rpy])
                 
                 if geo is not None:
-                    # Get the first geometry child (box, sphere, cylinder, mesh)
                     geo_type = None
                     geo_params = {}
                     for child in geo:
                         geo_type = child.tag
                         geo_params = child.attrib
-                        break  # Only process the first geometry element
+                        break
                     
                     if geo_type is not None:
-                        # Handle package:// paths if present
                         if geo_type == 'mesh' and 'filename' in geo_params:
                             geo_params['filename'] = geo_params['filename'].replace(
                                 'package://franka_description/', '')
@@ -51,6 +49,37 @@ class URDFProcessor:
             
             links[link_name] = {'collisions': collisions}
         return links
+    
+    def _parse_joints(self):
+        joints = {}
+        for joint in self.tree.findall('joint'):
+            joint_name = joint.get('name')
+            joint_type = joint.get('type')
+            
+            origin = joint.find('origin')
+            pos = [0, 0, 0]
+            rot = np.eye(3)
+            
+            if origin is not None:
+                xyz = origin.get('xyz', '0 0 0').split()
+                pos = [float(x) for x in xyz]
+                rpy = origin.get('rpy', '0 0 0').split()
+                rot = euler2mat(*[float(x) for x in rpy])
+            
+            axis = joint.find('axis')
+            joint_axis = [0, 0, 0]
+            if axis is not None:
+                joint_axis = [float(x) for x in axis.get('xyz', '0 0 0').split()]
+            
+            joints[joint_name] = {
+                'type': joint_type,
+                'parent': joint.find('parent').get('link'),
+                'child': joint.find('child').get('link'),
+                'position': pos,
+                'rotation': rot,
+                'axis': joint_axis
+            }
+        return joints
     
     def get_collision_shapes(self):
         """Returns simplified collision representation"""
