030cca3a1c7d9a6c367ff641ff26292154cc4488
LeyiYe <leyiasbarxas@gmail.com>
2025-04-28 18:41:26 +0100

    Update handSPH.py

--- handSPH.py
+++ handSPH.py
@@ -3,12 +3,13 @@ from pysph.solver.application import Application
 from pysph.solver.solver import Solver
 from pysph.sph.integrator import EPECIntegrator
 from pysph.sph.integrator_step import WCSPHStep, SolidMechStep
+from pysph.sph.rigid_body import RK2StepRigidBody
 from pysph.sph.equation import Group
 from pysph.sph.basic_equations import (
     ContinuityEquation, XSPHCorrection, SummationDensity
 )
 from pysph.sph.wc.basic import TaitEOS, MomentumEquation
-from pysph.base.utils import (get_particle_array, get_particle_array_rigid_body)
+from pysph.base.utils import get_particle_array_rigid_body
 from pysph.sph.solid_mech.basic import get_particle_array_elastic_dynamics
 from pysph.tools import geometry as G
 import numpy as np
@@ -49,16 +50,17 @@ class DeformableObjectWithGrippers(Application):
     def consume_user_options(self):
         self.nx = self.options.nx
         self.ny = int(self.nx * OBJECT_HEIGHT / OBJECT_WIDTH)
+        self.nz = int(self.nx * OBJECT_DEPTH / OBJECT_WIDTH)
         self.hdx = 1.2
         self.dx = OBJECT_WIDTH / self.nx
-        self.particle_mass = DENSITY * self.dx**2
+        self.particle_mass = DENSITY * self.dx**3
     
     def create_particles(self): 
         # First calculate the reference speed of sound
         c0 = np.sqrt(STIFFNESS/DENSITY)
         
-        # Create the deformable object using get_particle_array_elastic_dynamics
-        x, y, z= G.get_3d_block(
+        # Create the deformable object
+        x, y, z = G.get_3d_block(
             dx=self.dx, 
             length=OBJECT_WIDTH, 
             height=OBJECT_HEIGHT,
@@ -71,8 +73,6 @@ class DeformableObjectWithGrippers(Application):
         y += np.random.uniform(-self.dx/4, self.dx/4, len(y))
         z += np.random.uniform(-self.dx/4, self.dx/4, len(z))
         
-        particle_mass = (self.dx**3) * DENSITY
-
         # Create elastic object particle array
         object_pa = get_particle_array_elastic_dynamics(
             constants={
@@ -86,8 +86,9 @@ class DeformableObjectWithGrippers(Application):
             v=np.zeros_like(x),
             w=np.zeros_like(x),
             rho=np.ones_like(x)*DENSITY,
-            m=np.ones_like(x)*particle_mass,
+            m=np.ones_like(x)*self.particle_mass,
             h=np.ones_like(x)*self.dx*self.hdx,
+            # Stress tensor components
             s00=np.zeros_like(x),
             s01=np.zeros_like(x),
             s02=np.zeros_like(x),
@@ -97,21 +98,24 @@ class DeformableObjectWithGrippers(Application):
             s10=np.zeros_like(x),
             s20=np.zeros_like(x),
             s21=np.zeros_like(x),
-
-    
+            # Reference states
+            s000=np.zeros_like(x),
+            s010=np.zeros_like(x),
+            s020=np.zeros_like(x),
+            s110=np.zeros_like(x),
+            s120=np.zeros_like(x),
+            s220=np.zeros_like(x),
+            e=np.zeros_like(x),
+            e0=np.zeros_like(x),
+            # Time step properties
+            dt_cfl=np.zeros_like(x),
+            dt_force=np.zeros_like(x),
+            # Reference states
+            rho0=np.ones_like(x)*DENSITY,
+            u0=np.zeros_like(x),
+            v0=np.zeros_like(x),
+            w0=np.zeros_like(x)
         )
-        object_pa.add_property('dt_cfl')
-        object_pa.add_property('dt_force')
-        object_pa.dt_cfl[:] = 0.0  # Initialize to zero
-        object_pa.dt_force[:] = 0.0
-        object_pa.add_property('rho0')
-        object_pa.add_property('u0')
-        object_pa.add_property('v0')
-        object_pa.add_property('w0')
-        object_pa.rho0[:] = DENSITY
-        object_pa.u0[:] = 0.0
-        object_pa.v0[:] = 0.0
-        object_pa.w0[:] = 0.0
 
         # Create platform particles
         platform_x, platform_y, platform_z = G.get_3d_block(
@@ -119,7 +123,8 @@ class DeformableObjectWithGrippers(Application):
             length=BOX_WIDTH,
             height=PLATFORM_HEIGHT,
             depth=BOX_DEPTH,
-            center=[0, PLATFORM_HEIGHT/2, 0])
+            center=[0, PLATFORM_HEIGHT/2, 0]
+        )
         
         platform_pa = get_particle_array_rigid_body(
             name='platform',
@@ -128,16 +133,15 @@ class DeformableObjectWithGrippers(Application):
             m=np.ones_like(platform_x) * self.particle_mass,
             rho=np.ones_like(platform_x) * DENSITY * 100,
             cs=np.ones_like(platform_x) * c0 * 10,
+            # Reference states
             rho0=np.ones_like(platform_x) * DENSITY * 100,
             u0=np.zeros_like(platform_x),
             v0=np.zeros_like(platform_x),
-            w0=np.zeros_like(platform_x)
+            w0=np.zeros_like(platform_x),
+            # Time step properties
+            dt_cfl=np.zeros_like(platform_x),
+            dt_force=np.zeros_like(platform_x)
         )
-        platform_pa.add_property('dt_cfl')
-        platform_pa.add_property('dt_force')
-        platform_pa.dt_cfl[:] = 0.0
-        platform_pa.dt_force[:] = 0.0
-        
         # Create gripper particles (left and right)
         left_gripper_x, left_gripper_y, left_gripper_z = G.get_3d_block(
             dx=self.dx,
@@ -153,17 +157,16 @@ class DeformableObjectWithGrippers(Application):
             h=np.ones_like(left_gripper_x) * self.hdx * self.dx,
             m=np.ones_like(left_gripper_x) * self.particle_mass * 10,
             rho=np.ones_like(left_gripper_x) * DENSITY * 10,
-            cs=np.ones_like(left_gripper_x) * c0 * 10,    
-            rho0=np.ones_like(left_gripper_x) * DENSITY * 100,
+            cs=np.ones_like(left_gripper_x) * c0 * 10,
+            # Reference states
+            rho0=np.ones_like(left_gripper_x) * DENSITY * 10,
             u0=np.zeros_like(left_gripper_x),
             v0=np.zeros_like(left_gripper_x),
-            w0=np.zeros_like(left_gripper_x)
+            w0=np.zeros_like(left_gripper_x),
+            # Time step properties
+            dt_cfl=np.zeros_like(left_gripper_x),
+            dt_force=np.zeros_like(left_gripper_x)
         )
-        left_gripper_pa.add_property('dt_cfl')
-        left_gripper_pa.add_property('dt_force')
-        left_gripper_pa.dt_cfl[:] = 0.0
-        left_gripper_pa.dt_force[:] = 0.0
-        
         right_gripper_x, right_gripper_y, right_gripper_z = G.get_3d_block(
             dx=self.dx,
             length=GRIPPER_WIDTH,
@@ -179,24 +182,26 @@ class DeformableObjectWithGrippers(Application):
             m=np.ones_like(right_gripper_x) * self.particle_mass * 10,
             rho=np.ones_like(right_gripper_x) * DENSITY * 10,
             cs=np.ones_like(right_gripper_x) * c0 * 10,
-            rho0=np.ones_like(right_gripper_x) * DENSITY * 100,
+            # Reference states
+            rho0=np.ones_like(right_gripper_x) * DENSITY * 10,
             u0=np.zeros_like(right_gripper_x),
             v0=np.zeros_like(right_gripper_x),
-            w0=np.zeros_like(right_gripper_x)
-        )
-        
-        right_gripper_pa.add_property('dt_cfl')
-        right_gripper_pa.add_property('dt_force')
-        right_gripper_pa.dt_cfl[:] = 0.0
-        right_gripper_pa.dt_force[:] = 0.0
+            w0=np.zeros_like(right_gripper_x),
+            # Time step properties
+            dt_cfl=np.zeros_like(right_gripper_x),
+            dt_force=np.zeros_like(right_gripper_x))
 
         return [object_pa, platform_pa, left_gripper_pa, right_gripper_pa]
     
     def create_solver(self):
         kernel = CubicSpline(dim=DIM)
         
-        integrator = EPECIntegrator(object=SolidMechStep(), platform=SolidMechStep(),
-                                  left_gripper=SolidMechStep(), right_gripper=SolidMechStep())
+        integrator = EPECIntegrator(
+            object=SolidMechStep(),  # For deformable object
+            platform=RK2StepRigidBody(),  # For rigid platform
+            left_gripper=RK2StepRigidBody(),  # For rigid grippers
+            right_gripper=RK2StepRigidBody()
+        )
         
         solver = Solver(
             kernel=kernel,
@@ -241,7 +246,7 @@ class DeformableObjectWithGrippers(Application):
                     MomentumEquation(
                         dest='object', 
                         sources=['object', 'platform', 'left_gripper', 'right_gripper'],
-                        alpha=ALPHA, beta=BETA, gz=-9.81, c0 = np.sqrt(STIFFNESS/DENSITY)
+                        alpha=ALPHA, beta=BETA, gz=-9.81, c0=np.sqrt(STIFFNESS/DENSITY)
                     ),
                     XSPHCorrection(
                         dest='object', 
@@ -250,30 +255,26 @@ class DeformableObjectWithGrippers(Application):
                     )
                 ],
                 real=True
-            ),
-            
-            # Platform and grippers are treated as rigid bodies (no equations)
+            )
         ]
-        
         return equations
     
     def pre_step(self, solver):
-        # This gets called before each time step
         current_time = solver.t
         
         # Control gripper movement
         left_gripper = self.particles['left_gripper']
         right_gripper = self.particles['right_gripper']
         
-        # First phase: Close grippers
+        # First phase: Close grippers (0-1s)
         if current_time < 1.0:
             left_gripper.x[:] += GRIPPER_SPEED * DT
             right_gripper.x[:] -= GRIPPER_SPEED * DT
-        # Second phase: Lift grippers
+        
+        # Second phase: Lift grippers (1-3s)
         elif current_time < 3.0:
             left_gripper.y[:] += GRIPPER_SPEED * DT
             right_gripper.y[:] += GRIPPER_SPEED * DT
-        if current_time < 3.0:
             left_gripper.z[:] += GRIPPER_SPEED * DT 
             right_gripper.z[:] += GRIPPER_SPEED * DT
         
@@ -285,20 +286,19 @@ class DeformableObjectWithGrippers(Application):
         left_gripper.w[:] = GRIPPER_SPEED if current_time < 3.0 else 0.0
         right_gripper.w[:] = GRIPPER_SPEED if current_time < 3.0 else 0.0
         
-        # Simple plasticity model - yield stress
+        # Simple plasticity model
         object_pa = self.particles['object']
-        strain = np.sqrt(object_pa.du**2 + object_pa.dv**2) / STIFFNESS
+        strain = np.sqrt(object_pa.du**2 + object_pa.dv**2 + object_pa.dw**2) / STIFFNESS
         plastic = strain > YIELD_STRESS
         if np.any(plastic):
-            object_pa.u[plastic] *= 0.99  # Dampen velocity for plastic deformation
+            object_pa.u[plastic] *= 0.99
             object_pa.v[plastic] *= 0.99
+            object_pa.w[plastic] *= 0.99
     
     def post_step(self, solver):
-        # This gets called after each time step
         pass
     
     def post_process(self):
-        # This gets called after simulation completes
         pass
 
 if __name__ == '__main__':
