3cb8049268f32ff627328693f5f3a5df077f6923
LeyiYe <leyiasbarxas@gmail.com>
2025-04-29 19:07:22 +0100

    Update DefObjSPH.py

--- DefObjSPH.py
+++ DefObjSPH.py
@@ -61,7 +61,7 @@ class GraspDeformableBlock(Application):
         block.add_property('arho'); block.arho[:] = 1.0/self.rho0
         block.add_property('cs');   block.cs[:]   = self.c0
 
-        for arr in ['as22', 'as22', 'as12','as00', 'as11', 'as01']:
+        for arr in ['as22', 'as22', 'as12','as00', 'as11', 'as01', 'as02']:
             block.add_property(arr)
             
 
