274d166958b123c9b4b9f588d88a5e6eab2925d0
LeyiYe <leyiasbarxas@gmail.com>
2025-04-29 15:14:56 +0100

    Update DefObjSPH.py

--- DefObjSPH.py
+++ DefObjSPH.py
@@ -206,13 +206,7 @@ class GraspDeformableBlock(Application):
             block.z[mask] = floor_z
             block.u[mask] = 0.0
             block.v[mask] = 0.0
-            block.w[mask] = 0.0 = self.platform_size[2] + 0.5 * self.dx
-        mask = block.z < floor_z
-        if mask.any():
-            block.z[mask] = floor_z
-            block.u[mask] = 0.0
-            block.v[mask] = 0.0
-            block.w[mask] = 0.0
+            block.w[mask] = 0.0 
 
 if __name__ == '__main__':
     app = GraspDeformableBlock()
