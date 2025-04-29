ce2750cea8fcfe7d79ec4315f8a9c413a89ee7d4
LeyiYe <leyiasbarxas@gmail.com>
2025-04-29 23:35:20 +0100

    Update handSPH.py

--- handSPH.py
+++ handSPH.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
-import os
 import numpy as np
-
-from pysph.base.utils      import get_particle_array
-from pysph.solver.application  import Application
-from pysph.sph.scheme       import SchemeChooser
-from pysph.sph.solid_mech.basic import ElasticSolidsScheme, get_particle_array_elastic_dynamics
-from pysph.sph.equation     import Group
-from pysph.sph.basic_equations import BodyForce
+from pysph.base.utils             import get_particle_array
+from pysph.solver.application     import Application
+from pysph.sph.scheme             import SchemeChooser
+from pysph.sph.solid_mech.basic   import (
+    ElasticSolidsScheme,
+    get_particle_array_elastic_dynamics
+)
+from pysph.sph.equation           import Group
+from pysph.sph.basic_equations    import BodyForce
 
 class GraspDeformableBlock(Application):
     def initialize(self):
-        # Simulation parameters
+        # core sim parameters
         self.dim    = 3
-        self.dx     = 0.01           # particle spacing
-        self.hdx    = 1.3            # smoothing length factor
-        self.rho0   = 1000.0         # reference density
-        self.E      = 1e7            # Young's modulus (Pa)
-        self.nu     = 0.49           # Poisson's ratio
-        self.c0     = 50.0           # speed of sound
+        self.dx     = 0.01
+        self.hdx    = 1.3
+        self.rho0   = 1000.0
+        self.E      = 1e7      # block stiffness
+        self.nu     = 0.49
+        self.c0     = 50.0
 
-        # Geometry dimensions (m)
+        # geometry (m)
         self.block_size    = (0.3, 0.2, 0.1)
         self.platform_size = (1.0, 0.6, 0.02)
         self.gripper_size  = (0.05, 0.1, 0.2)
 
     def create_block(self, center, size):
-        # build a regular grid of points inside box
         nx = max(3, int(round(size[0]/self.dx)))
         ny = max(3, int(round(size[1]/self.dx)))
         nz = max(3, int(round(size[2]/self.dx)))
@@ -40,46 +40,44 @@ class GraspDeformableBlock(Application):
         return x.ravel(), y.ravel(), z.ravel()
 
     def create_particles(self):
-        # --- deformable block (auto-adds only what ElasticSolidsScheme needs) ---
+        # --- deformable block ---
         bx, by, bz = self.create_block(
             center=(0, 0, self.platform_size[2] + self.block_size[2]/2),
             size=self.block_size
         )
-        consts = {
-            'E'      : self.E,
-            'nu'     : self.nu,
-            'rho_ref': self.rho0,
-            'c0_ref' : self.c0,
-        }
         block = get_particle_array_elastic_dynamics(
             name='block',
             x=bx, y=by, z=bz,
             h=self.hdx*self.dx,
             m=self.dx**3 * self.rho0,
             rho=self.rho0,
-            constants=consts
-        )  # :contentReference[oaicite:0]{index=0}
+            constants={
+                'E'      : self.E,
+                'nu'     : self.nu,
+                'rho_ref': self.rho0,
+                'c0_ref' : self.c0
+            }
+        )
 
-        # --- rigid platform & grippers (minimal + stress arrays) ---
+        # helper to build a “rigid” platform/gripper as a super-stiff solid
         def make_rigid(name, center, size):
-            x, y, z = self.create_block(center, size)
-            pa = get_particle_array(
+            x,y,z = self.create_block(center, size)
+            # boost stiffness & mass so it barely moves
+            stiff_E = 1e9
+            mass_factor = 1e4
+            return get_particle_array_elastic_dynamics(
                 name=name,
-                x=x,  y=y,  z=z,
-                h=0.02*2.0,
-                m=1e12,
+                x=x, y=y, z=z,
+                h=self.hdx*self.dx,
+                m=self.dx**3 * self.rho0 * mass_factor,
                 rho=self.rho0,
-                is_boundary=1,
-                is_rigid=1,
-                arho=1.0/self.rho0,
-                cs=self.c0,
-                z0=z.copy()
+                constants={
+                    'E'      : stiff_E,
+                    'nu'     : self.nu,
+                    'rho_ref': self.rho0,
+                    'c0_ref' : self.c0
+                }
             )
-            # add only the 6 independent r_ij and 6 s_ij needed by momentum eqn
-            for i,j in [(0,0),(0,1),(0,2),(1,1),(1,2),(2,2)]:
-                pa.add_property(f'r{i}{j}')
-                pa.add_property(f's{i}{j}')
-            return pa
 
         platform = make_rigid(
             'platform',
@@ -101,8 +99,8 @@ class GraspDeformableBlock(Application):
 
     def create_scheme(self):
         elastic = ElasticSolidsScheme(
-            elastic_solids=['block'],
-            solids=['platform','gripper1','gripper2'],
+            elastic_solids=['block', 'platform', 'gripper1', 'gripper2'],
+            solids=[],
             dim=self.dim,
             artificial_stress_eps=0.5,
             xsph_eps=0.5
@@ -110,11 +108,11 @@ class GraspDeformableBlock(Application):
         return SchemeChooser(default='elastic', elastic=elastic)
 
     def configure_scheme(self):
-        self.scheme.configure_solver(dt=5e-5, tf=2.0, pfreq=50)
+        self.scheme.configure_solver(dt=5e-3, tf=2.0, pfreq=50)
 
     def create_equations(self):
         eqns = self.scheme.get_equations()
-        # gravity on the deformable block
+        # only apply gravity to the deformable block
         eqns.append(Group(equations=[
             BodyForce(dest='block', sources=None,
                       fx=0, fy=0, fz=-9.81)
@@ -122,10 +120,9 @@ class GraspDeformableBlock(Application):
         return eqns
 
     def post_step(self, solver):
-        # open/close logic for grippers
+        # your gripper open/close logic
         g1, g2 = self.particles[2], self.particles[3]
         dt = solver.dt
-
         half_block = 0.5*self.block_size[0]
         half_grip  = 0.5*self.gripper_size[0]
         target = -half_block - half_grip + 0.02
@@ -139,7 +136,6 @@ class GraspDeformableBlock(Application):
             g1.u[:] = g2.u[:] = 0.0
             g1.w[:] = g2.w[:] = 0.3
 
-        # update rigid‐body positions
         for gr in (g1, g2):
             gr.x  += gr.u * dt
             gr.y  += gr.v * dt
